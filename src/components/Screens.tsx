372f1cb23ccc6c428c487272ac53f6c83bbccfeb
Seven <michalwww3@gmail.com>
2020-11-02 09:51:13 +0100

    Navigation tab switching

--- src/components/Screens.tsx
+++ src/components/Screens.tsx
@@ -8,12 +8,74 @@ import "./Style.scss";
 
 export default function Screens(): JSX.Element {
   const [screen, setScreen] = useState<JSX.Element>(<Landing />);
-  const navbarItem: string = "navbar-item";
-  const navbarItemActive: string = "navbar-item-active";
-  const [navbarItemClass, setNavbarItemClass] = useState<string>(navbarItem);
+
+  const [homeItemClass, setHomeItemClass] = useState<string>("navbar-item");
+  const [projectsItemClass, setProjectsItemClass] = useState<string>(
+    "navbar-item"
+  );
+  const [technologiesItemClass, setTechnologiesItemClass] = useState<string>(
+    "navbar-item"
+  );
+  const [aboutItemClass, setAboutItemClass] = useState<string>("navbar-item");
+  const [contactItemClass, setContactItemClass] = useState<string>(
+    "navbar-item"
+  );
+
   const [isBurgerActive, setIsBurgerActive] = useState<boolean>(false);
 
-  const onClickHandler = () => {};
+  // const resetItemsClass = (activeItem: string): void => {
+  //   const array = ["Home", "Projects", "Technologies", "About", "Contact"]
+  //   setHomeItemClass("navbar-item-active");
+  //   setProjectsItemClass("navbar-item");
+  //   setTechnologiesItemClass("navbar-item");
+  //   setAboutItemClass("navbar-item");
+  //   setContactItemClass("navbar-item");
+  // };
+
+  const onClickHandler = (tab: string) => {
+    switch (tab) {
+      case "Home":
+        setScreen(<Landing />);
+        setHomeItemClass("navbar-item-active");
+        setProjectsItemClass("navbar-item");
+        setTechnologiesItemClass("navbar-item");
+        setAboutItemClass("navbar-item");
+        setContactItemClass("navbar-item");
+        break;
+      case "Projects":
+        setScreen(<Projects />);
+        setProjectsItemClass("navbar-item-active");
+        setHomeItemClass("navbar-item");
+        setTechnologiesItemClass("navbar-item");
+        setAboutItemClass("navbar-item");
+        setContactItemClass("navbar-item");
+        break;
+      case "Technologies":
+        setScreen(<Technologies />);
+        setTechnologiesItemClass("navbar-item-active");
+        setProjectsItemClass("navbar-item");
+        setHomeItemClass("navbar-item");
+        setAboutItemClass("navbar-item");
+        setContactItemClass("navbar-item");
+        break;
+      case "About":
+        setScreen(<About />);
+        setAboutItemClass("navbar-item-active");
+        setProjectsItemClass("navbar-item");
+        setTechnologiesItemClass("navbar-item");
+        setHomeItemClass("navbar-item");
+        setContactItemClass("navbar-item");
+        break;
+      case "Contact":
+        setScreen(<Contact />);
+        setContactItemClass("navbar-item-active");
+        setProjectsItemClass("navbar-item");
+        setTechnologiesItemClass("navbar-item");
+        setAboutItemClass("navbar-item");
+        setHomeItemClass("navbar-item");
+        break;
+    }
+  };
 
   return (
     <div className="Screens">
@@ -32,39 +94,39 @@ export default function Screens(): JSX.Element {
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
-            <button
-              className={navbarItemClass}
+            <p
+              className={homeItemClass}
               onClick={(): void => setScreen(<Landing />)}>
               Home
-            </button>
+            </p>
           </button>
         </div>
         <div className="navbar-menu">
           <div className="navbar-start"></div>
           <div className="navbar-end">
             <button
-              className={navbarItemClass}
-              onClick={(): void => setScreen(<Landing />)}>
+              className={homeItemClass}
+              onClick={(): void => onClickHandler("Home")}>
               Home
             </button>
             <button
-              className={navbarItemClass}
-              onClick={(): void => setScreen(<Projects />)}>
+              className={projectsItemClass}
+              onClick={(): void => onClickHandler("Projects")}>
               Projects
             </button>
             <button
-              className={navbarItemClass}
-              onClick={(): void => setScreen(<Technologies />)}>
+              className={technologiesItemClass}
+              onClick={(): void => onClickHandler("Technologies")}>
               Technologies
             </button>
             <button
-              className={navbarItemClass}
-              onClick={(): void => setScreen(<About />)}>
+              className={aboutItemClass}
+              onClick={(): void => onClickHandler("About")}>
               About
             </button>
             <button
-              className={navbarItemClass}
-              onClick={(): void => setScreen(<Contact />)}>
+              className={contactItemClass}
+              onClick={(): void => onClickHandler("Contact")}>
               Contact
             </button>
           </div>
