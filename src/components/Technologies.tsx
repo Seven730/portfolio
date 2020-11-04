753fcf5fbafd8c21df75a66cdb486f9cdf1bdd48
Seven <michalwww3@gmail.com>
2020-11-04 11:44:13 +0100

    Technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Technologies() {
-  return <div className="Technologies"></div>;
+  return (
+    <div className="Technologies">
+      <div className="technologies-container">
+        <div className="site-technologies-container">
+          <header>This site uses:</header>
+          <ul>
+            <li>React</li>
+            <li>Bulma</li>
+            <li>SCSS</li>
+          </ul>
+        </div>
+        <div className="used-technologies-container">
+          <header>Other technologies I use:</header>
+          <ul>
+            <li>Angular</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
